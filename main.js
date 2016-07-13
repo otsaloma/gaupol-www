6f05b61ee3756646ad4e06c687da71afe4ac5ec9
Osmo Salomaa <otsaloma@iki.fi>
2016-07-13 15:19:11 +0300

    Avoid multiple download buttons flickering

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // -*- coding: utf-8-unix -*-
 
 $(document).on("ready", function() {
-    platform.os.family.indexOf("Windows") === 0 ?
-        $(".download.source").hide() :
-        $(".download.windows").hide();
-
+    if (platform.os.family.indexOf("Windows") === 0) {
+        $(".download.source").hide();
+        $(".download.windows").show();
+    }
 });
